03811698c01b4a833f9dd15d9496227a299ad437
csplatti <csplatti@gmaillcom>
2025-06-20 21:56:18 +0000

    first attempt at parallel reduction, incorrect result

--- parallel_reduction/pr.cu
+++ parallel_reduction/pr.cu
@@ -1,11 +1,68 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-__global__ void parrallelReduce(int *nums) {
-    
+__global__ void parallelSum(int *nums, int N) {
+    int i = threadIdx.x;
+
+    // printf("Thread id: %d\n", i);
+
+    float indexJump = N / 2;
+    int roundedJump = ceilf(indexJump);
+
+
+    if (2 * i <= N) {
+        nums[i] += nums[i+roundedJump];
+        // printf("i: %d; 2i: %d\n", nums[i], nums[2*i]);
+    }
+
+    float nextSize = N / 2;
+    int nextN = ceilf(nextSize);
+    if (nextN > 1 && i == 0) {
+        // printf("Nextn: %d\n", nextN);
+        parallelSum<<<1, nextN>>>(nums, nextN); // this runs in each thread: that is bad
+    }
+}
+
+// REQUIRES: N == length of nums
+void serialSum(int nums[], int numsSize, int *serialOut) {
+    int sum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        sum += nums[i];
+    }
+
+    *serialOut = sum;
 }
 
 int main() {
+    int nums[20];
+    int numsSize = sizeof(nums) / sizeof(int);
 
+    for (int i = 0; i < numsSize; i++) {
+        nums[i] = i+1;
+        // cout << nums[i] << endl;
+    }
+
+    int serialSumResult = 0;
+
+    serialSum(nums, numsSize, &serialSumResult);
+
+    cout << "Serial: " << serialSumResult << endl;
+
+    int parallelSumResult = 0;
+
+    int *pn = 0;
+
+    cudaMalloc(&pn, sizeof(nums));
+    cudaMemcpy(pn, nums, sizeof(nums), cudaMemcpyHostToDevice);
+
+    int numThreads = ceil(numsSize / 2);
+
+    parallelSum<<<1, numThreads>>>(pn, numsSize);
+
+    cudaMemcpy(nums, pn, sizeof(nums), cudaMemcpyDeviceToHost);
+    parallelSumResult = nums[0];
+
+    cout << "Parallel: " << parallelSumResult << endl;
 }
