04ce25f1a4216064e2431c057f2fae00dd56c0eb
csplatti <csplatti@gmail.com>
2025-07-02 22:33:39 +0000

    code cleanup

--- parallel_reduction/pr.cu
+++ parallel_reduction/pr.cu
@@ -7,24 +7,10 @@ using namespace std;
 __global__ void parallelSum(int *nums, int N) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
 
-    // printf("%d\n", i);
-
-    // printf("Thread id: %d\n", i);
-
-    // if (i == 0) {
-    //     for (int j = 0; j < N; j++) {
-    //         printf("%d ", nums[j]);
-    //     }
-    //     printf("\n");
-    //     printf("N: %d\n", N);
-    // }
-
-    float indexJump = (float) N / 2;
-    int roundedJump = ceilf(indexJump);
+    int roundedJump = ceilf((float) N / 2);
 
     if (i + roundedJump < N) {
         nums[i] += nums[i+roundedJump];
-        // printf("i: %d; 2i: %d\n", nums[i], nums[2*i]);
     }
 
     float nextSize = (float) N / (float) 2;
@@ -47,18 +33,7 @@ void serialSum(int nums[], int numsSize, long *serialOut) {
     *serialOut = sum;
 }
 
-int main() {
-    int N; 
-    // cout << "Enter the number you would like to sum to (0 < N < 65535): ";
-    cin >> N;
-    int nums[N];
-    int numsSize = sizeof(nums) / sizeof(int);
-
-    for (int i = 0; i < numsSize; i++) {
-        nums[i] = i+1;
-        // cout << nums[i] << endl;
-    }
-
+void runSerial(int nums[], int numsSize) {
     long serialSumResult = 0;
 
     auto startSerial = chrono::high_resolution_clock::now();
@@ -67,33 +42,47 @@ int main() {
 
     auto serialTime = chrono::duration_cast<chrono::milliseconds>(endSerial-startSerial);
 
-    // cout << "Serial: " << serialSumResult << " Runtime: " << serialTime.count() << "ms" << endl;
+    cout << "Serial: " << serialSumResult << " Runtime: " << serialTime.count() << "ms" << endl;
+}
 
-    cout << serialTime.count() << endl;
-
+void runParallel(int nums[], int numsSize) {
     int parallelSumResult = 0;
 
     int *pn = 0;
 
-    cudaMalloc(&pn, sizeof(nums));
-    cudaMemcpy(pn, nums, sizeof(nums), cudaMemcpyHostToDevice);
+    cudaMalloc(&pn, numsSize * sizeof(int));
+    cudaMemcpy(pn, nums, numsSize * sizeof(int), cudaMemcpyHostToDevice);
 
     int numThreads = ceil(numsSize / 2);
 
-    // printf("%d\n", (int) ceil((float) numsSize / 1024));
-
     auto startParallel = chrono::high_resolution_clock::now();
     parallelSum<<<(int) ceil((float) numsSize / 1024), 1024>>>(pn, numsSize);
+    cudaDeviceSynchronize();
     auto endParallel = chrono::high_resolution_clock::now();
 
     auto parallelTime = chrono::duration_cast<chrono::milliseconds>(endParallel - startParallel);
 
-    cudaMemcpy(nums, pn, sizeof(nums), cudaMemcpyDeviceToHost);
+    cudaMemcpy(nums, pn, numsSize * sizeof(int), cudaMemcpyDeviceToHost);
     parallelSumResult = nums[0];
 
-    // cout << "Parallel: " << parallelSumResult << " Runtime: " << parallelTime.count() << "ms" << endl;
+    cout << "Parallel: " << parallelSumResult << " Runtime: " << parallelTime.count() << "ms" << endl;
 
-    cout << parallelTime.count() << endl;
+    // cout << parallelTime.count() << endl;
 
     cudaFree(&pn);
+}
+
+int main() {
+    int N = 65535;
+    // cout << "Enter the number you would like to sum to (0 < N < 65535): ";
+    // cin >> N;
+    int nums[N];
+    int numsSize = sizeof(nums) / sizeof(int);
+
+    for (int i = 0; i < numsSize; i++) {
+        nums[i] = i+1;
+    }
+
+    runSerial(nums, numsSize);
+    runParallel(nums, numsSize);
 }
