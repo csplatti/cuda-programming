b3f02c693a25a9714a4d98c2b09543423c70121c
csplatti <csplatti@gmail.com>
2025-07-03 00:04:03 +0000

    made it even worse

--- parallel_reduction/pr.cu
+++ parallel_reduction/pr.cu
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-__global__ void parallelSum(int *nums, int N) {
+__global__ void parallelSum1(int *nums, int N) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
 
     int roundedJump = ceilf((float) N / 2);
@@ -19,13 +19,13 @@ __global__ void parallelSum(int *nums, int N) {
     int nextNThreads = ceilf(nextN / 2);
     if (nextN > 1 && i == 0) {
         // printf("Nextn: %d\n", nextN);
-        parallelSum<<<ceil((float) nextN / 1024), 1024>>>(nums, nextN); // this runs in each thread: that is bad
+        // parallelSum1<<<ceil((float) nextN / 1024), 1024>>>(nums, nextN); // this runs in each thread: that is bad
     }
 }
 
 // REQUIRES: N == length of nums
-void serialSum(int nums[], int numsSize, long *serialOut) {
-    long sum = 0;
+void serialSum(int nums[], int numsSize, long long *serialOut) {
+    long long sum = 0;
     for (int i = 0; i < numsSize; i++) {
         sum += nums[i];
     }
@@ -34,7 +34,7 @@ void serialSum(int nums[], int numsSize, long *serialOut) {
 }
 
 void runSerial(int nums[], int numsSize) {
-    long serialSumResult = 0;
+    long long serialSumResult = 0;
 
     auto startSerial = chrono::high_resolution_clock::now();
     serialSum(nums, numsSize, &serialSumResult);
@@ -45,31 +45,73 @@ void runSerial(int nums[], int numsSize) {
     cout << "Serial: " << serialSumResult << " Runtime: " << serialTime.count() << "ms" << endl;
 }
 
+__global__ void parallelSum2(int *nums, long long *sums, int numsSize, int additionIndex) {
+    int i = threadIdx.x;
+
+    if (additionIndex == 0 && i < numsSize) {
+        sums[i] = nums[i];
+    }
+
+    if (i + additionIndex * 1024 < numsSize) {
+        sums[i] += nums[i + additionIndex * 1024];
+    }
+}
+
+long long parallel2Host(int nums[], int numsLength) {
+    int n = numsLength;
+    int numAdds = ceil((float) n / 1024);
+    long long sums[1024];
+    
+    long long *s = 0;
+    int *np = 0;
+
+    cudaMalloc(&s, sizeof(sums));
+    cudaMalloc(&np, numsLength * sizeof(int));
+    cudaMemcpy(s, sums, sizeof(sums), cudaMemcpyDeviceToHost);
+    cudaMemcpy(np, nums, numsLength * sizeof(int), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < numAdds; i++) {
+        parallelSum2<<<1, 1024>>>(np, s, numsLength, i);
+    }
+
+    cudaMemcpy(sums, s, sizeof(sums), cudaMemcpyHostToDevice);
+    cudaMemcpy(nums, np, numsLength * sizeof(int), cudaMemcpyHostToDevice);
+    cudaFree(np);
+    cudaFree(s);
+
+    long long sum = 0;
+    for (int i = 0; i < sizeof(sums) / sizeof(long long); i++) {
+        sum += sums[i];
+    }
+
+    return sum;
+}
+
 void runParallel(int nums[], int numsSize) {
     int parallelSumResult = 0;
 
-    int *pn = 0;
+    // Allocate Memory and Copy to Device
+    // int *pn = 0;
+    // cudaMalloc(&pn, numsSize * sizeof(int));
+    // cudaMemcpy(pn, nums, numsSize * sizeof(int), cudaMemcpyHostToDevice);
 
-    cudaMalloc(&pn, numsSize * sizeof(int));
-    cudaMemcpy(pn, nums, numsSize * sizeof(int), cudaMemcpyHostToDevice);
-
-    int numThreads = ceil(numsSize / 2);
-
+    // Congifure and Run Kernel
+    // int numThreads = ceil(numsSize / 2);
     auto startParallel = chrono::high_resolution_clock::now();
-    parallelSum<<<(int) ceil((float) numsSize / 1024), 1024>>>(pn, numsSize);
+    parallelSumResult = parallel2Host(nums, numsSize);
     cudaDeviceSynchronize();
     auto endParallel = chrono::high_resolution_clock::now();
 
     auto parallelTime = chrono::duration_cast<chrono::milliseconds>(endParallel - startParallel);
 
-    cudaMemcpy(nums, pn, numsSize * sizeof(int), cudaMemcpyDeviceToHost);
-    parallelSumResult = nums[0];
+    // cudaMemcpy(nums, pn, numsSize * sizeof(int), cudaMemcpyDeviceToHost);
+    // parallelSumResult = nums[0];
 
     cout << "Parallel: " << parallelSumResult << " Runtime: " << parallelTime.count() << "ms" << endl;
 
     // cout << parallelTime.count() << endl;
 
-    cudaFree(&pn);
+    // cudaFree(&pn);
 }
 
 int main() {
@@ -81,8 +123,11 @@ int main() {
 
     for (int i = 0; i < numsSize; i++) {
         nums[i] = i+1;
+        // printf("%d\n", i);
     }
+    // printf("done with for loop to create array");
 
     runSerial(nums, numsSize);
     runParallel(nums, numsSize);
+
 }
