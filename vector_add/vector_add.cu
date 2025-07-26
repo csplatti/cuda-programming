4744aecbdf1edd2615715f894f7ecf473ed52cb5
csplatti <csplatti@gmail.com>
2025-07-26 00:52:20 +0000

    serial and parallel vector sums implemented with runtime measurements

--- vector_add/vector_add.cu
+++ vector_add/vector_add.cu
@@ -1,48 +1,83 @@
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 
-__global__ void addVectors(int *a, int *b, int *c, int *n) {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+__global__ void addVectorsParallel(int *a, int *b, int *c, int N) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i < N) {
+        c[i] = a[i] + b[i];
+    }
+}
 
-    n[i] = sizeof(c) / sizeof(int); 
-    // this is not what you are expecting it to be because you cannot find the length of an array from a pointer!
+void addVectorsSerial(int *a, int *b, int *c, int vectorLength) {
+    for (int i = 0; i < vectorLength; i++) {
+        c[i] = a[i] + b[i];
+    }
 }
 
 int main() {
-    int a[] = {1, 2, 3, 4, 5};
-    int b[] = {5, 4, 3, 2, 1};
-    int c[] = {0, 0, 0, 0, 0};
-    int n[] = {0, 0, 0, 0, 0};
+    // Get Vector Size Input
+    int N;
+    cout << "Please Enter Desired Vector Size: " << endl; 
+    cin >> N;
 
+    // Vector Setup
+    int a[N];
+    int b[N];
+    int serialOut[N];
+    int parallelOut[N];
+
+    // Fill Vectors
+    for (int i = 0; i < N; i++) {
+        a[i] = rand() % 501;
+        b[i] = rand() % 501;
+    }
+
+    // Run Serial Sum
+    auto startSerial = chrono::high_resolution_clock::now();
+    addVectorsSerial(a, b, serialOut, N);
+    auto endSerial = chrono::high_resolution_clock::now();
+
+    // calculate and Display Serial Sum Runtime
+    auto serialTime = chrono::duration_cast<chrono::microseconds>(endSerial - startSerial).count();
+
+    cout << "Serial Runtime: " << serialTime << " microseconds" << endl;
+    // cout << serialTime << endl;
+
+    // GPU Memory Management Setup
     int *pa = 0;
     int *pb = 0;
-    int *pc = 0;
-    int *pn = 0;
+    int *pOut = 0;
 
     cudaMalloc(&pa, sizeof(a));
     cudaMalloc(&pb, sizeof(b));
-    cudaMalloc(&pc, sizeof(c));
-    cudaMalloc(&pn, sizeof(n));
+    cudaMalloc(&pOut, sizeof(parallelOut));
 
     cudaMemcpy(pa, a, sizeof(a), cudaMemcpyHostToDevice);
     cudaMemcpy(pb, b, sizeof(b), cudaMemcpyHostToDevice);
-    cudaMemcpy(pc, c, sizeof(c), cudaMemcpyHostToDevice);
-    cudaMemcpy(pn, n, sizeof(n), cudaMemcpyHostToDevice);
+    cudaMemcpy(pOut, parallelOut, sizeof(parallelOut), cudaMemcpyHostToDevice);
 
-    addVectors<<<1, sizeof(c)>>>(pa, pb, pc, pn);
+    // Run Parallel Sum
+    auto startParallel = chrono::high_resolution_clock::now();
+    addVectorsParallel<<<sizeof(parallelOut) / 1024 + 1, 1024>>>(pa, pb, pOut, N);
+    cudaDeviceSynchronize();
+    auto endParallel = chrono::high_resolution_clock::now();
 
-    cudaMemcpy(c, pc, sizeof(c), cudaMemcpyDeviceToHost);
-    cudaMemcpy(n, pn, sizeof(n), cudaMemcpyDeviceToHost);
+    // Calculate and Display Parallel Sum Runtime
+    auto parallelTime = chrono::duration_cast<chrono::microseconds>(endParallel - startParallel).count();
 
-    for (int i = 0; i < sizeof(c) / sizeof(int); i++) {
-        cout << c[i];
+    cout << "Parallel Runtime: " << parallelTime << " microseconds" << endl;
+    // cout << parallelTime << endl;
+
+    // Copy Parallel Sum Result Back from GPU
+    cudaMemcpy(parallelOut, pOut, sizeof(parallelOut), cudaMemcpyDeviceToHost);
+
+    // Error Detection
+    for (int i = 0; i < N; i++) {
+        if (parallelOut[i] != serialOut[i]) {
+            cout << "ERROR AT " << i << endl;
+            break;
+        }
     }
-    cout << endl;
-
-    for (int i = 0; i < sizeof(n) / sizeof(int); i++) {
-        cout << n[i];
-    }
-    cout << endl;
 }
